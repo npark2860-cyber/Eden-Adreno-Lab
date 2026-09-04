225d95eb1775c7f68611626a17bddc98cb97f099
npark2860-cyber <npark2860@gmail.com>
2026-09-04 17:00:32 +0900

    imp-006: add x18 fallback classifier and state bridge API

---
+++ src/core/arm/nce/x18_fallback.h
@@ -0,0 +1,44 @@
+// SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#include "common/common_types.h"
+#include "core/arm/arm_interface.h"
+
+namespace Kernel {
+class KThread;
+}
+
+namespace Core {
+
+class ArmDynarmic64;
+struct GuestContext;
+
+namespace NCE {
+
+enum class X18InstructionClass : u8 {
+    NoX18,
+    SupportedOrdinary,
+    ExcludedExclusive,
+    Unsupported,
+};
+
+struct X18FallbackStepResult {
+    bool completed{};
+    HaltReason halt_reason{};
+};
+
+class X18Fallback {
+public:
+    static constexpr u32 NzcvMask = 0xF0000000u;
+
+    [[nodiscard]] static X18InstructionClass ClassifyInstruction(u32 instruction);
+
+    [[nodiscard]] static X18FallbackStepResult Step(ArmDynarmic64& backend,
+                                                    Kernel::KThread* thread,
+                                                    GuestContext& guest);
+};
+
+} // namespace NCE
+} // namespace Core
