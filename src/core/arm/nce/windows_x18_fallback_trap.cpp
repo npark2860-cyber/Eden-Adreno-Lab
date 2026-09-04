3466f516769e18c058c75263a1ed87e82fdc62c3
npark2860-cyber <npark2860@gmail.com>
2026-09-04 22:20:19 +0900

    imp-006: validate tagged x18 trap metadata

--- src/core/arm/nce/windows_x18_fallback_trap.cpp
+++ src/core/arm/nce/windows_x18_fallback_trap.cpp
@@ -9,16 +9,21 @@
 namespace Core::NCE {
 
 std::optional<u32> WindowsX18FallbackTrap::FindOriginalInstruction(
-    u64 pc, const X18FallbackSiteMap& sites) noexcept {
-    const auto it = sites.find(pc);
-    if (it == sites.end()) {
+    u64 pc, const X18FallbackMetadata& metadata) noexcept {
+    const auto it = metadata.find(X18SitePatcher::MetadataKey(pc));
+    if (it == metadata.end()) {
         return std::nullopt;
     }
-    return it->second;
+
+    const u64 value = it->second;
+    if (static_cast<u32>(value >> 32) != X18SitePatcher::MetadataMagic) {
+        return std::nullopt;
+    }
+    return static_cast<u32>(value);
 }
 
 bool WindowsX18FallbackTrap::TryRedirect(PEXCEPTION_POINTERS exception, GuestContext& guest,
-                                         const X18FallbackSiteMap& sites) noexcept {
+                                         const X18FallbackMetadata& metadata) noexcept {
     if (exception == nullptr || exception->ExceptionRecord == nullptr ||
         exception->ContextRecord == nullptr ||
         exception->ExceptionRecord->ExceptionCode != EXCEPTION_BREAKPOINT) {
@@ -26,7 +31,7 @@ bool WindowsX18FallbackTrap::TryRedirect(PEXCEPTION_POINTERS exception, GuestCon
     }
 
     auto& context = *reinterpret_cast<ARM64_NT_CONTEXT*>(exception->ContextRecord);
-    if (!FindOriginalInstruction(context.Pc, sites).has_value()) {
+    if (!FindOriginalInstruction(context.Pc, metadata).has_value()) {
         return false;
     }
 
