0ff398d6afefd25c74fb1ac74b0b20c63ddc7e71
npark2860-cyber <npark2860@gmail.com>
2026-09-22 23:25:59 +0900

    diag: add one-shot post-audio NCE reentry probe

---
+++ src/common/windows_nce_post_audio_probe.h
@@ -0,0 +1,28 @@
+// SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#include <atomic>
+
+namespace Common::WindowsNcePostAudioProbe {
+
+inline std::atomic<unsigned> g_stage{0};
+
+inline void Arm() noexcept {
+    g_stage.store(1, std::memory_order_release);
+}
+
+inline bool ObserveRunThread() noexcept {
+    unsigned expected = 1;
+    return g_stage.compare_exchange_strong(expected, 2, std::memory_order_acq_rel,
+                                           std::memory_order_acquire);
+}
+
+inline bool ObserveEnterGuest() noexcept {
+    unsigned expected = 2;
+    return g_stage.compare_exchange_strong(expected, 3, std::memory_order_acq_rel,
+                                           std::memory_order_acquire);
+}
+
+} // namespace Common::WindowsNcePostAudioProbe
